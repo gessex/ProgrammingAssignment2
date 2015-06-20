59c239b87b671b24b86156ac4f5a7b87e542e77d
gessex <graham.essex@pepsico.com>
2015-06-20 12:06:23 +0100

    R Prog Assignment 2
    Assignment 2 from the R Programming course. Week 3.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        ## the 'set' option from example hashed out as 
+        ## not required for submission 
+        ## set <- function(y) {
+        ##         x <<- y
+        ##         s <<- NULL
+        ## }
+        #get used to show the matrix values entered
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(get = get,
+             ## set=set ##not required for submission
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## If the result 's' is already cached in 'getsolve, return from cache
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        ## otherwise calculate new value for 's' into 'data', and store
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
